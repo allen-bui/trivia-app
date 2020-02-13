785afa6d2264ed01df409a05a22fb1393842a10b
allen-bui <allen.vbui@gmail.com>
2020-02-12 17:25:11 -0800

    completes welcome page mvp

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Welcome from './components/welcome/welcome';
 import './App.scss';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      hello world
-    </div>
-  );
+
+  const [isHome, setIsHome] = useState<boolean>(true);
+  const [value, setValue] = useState<number>(5);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const numberValue: number = Number(event.target.value);
+    setValue(numberValue);
+  }
+
+  const handleSubmit = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    event.preventDefault();
+    setIsHome(false);
+  }
+
+  if (isHome) {
+    return <Welcome handleChange={handleChange} handleSubmit={handleSubmit} />
+  } else {
+    console.log(value);
+    return <div>this is the trivia app!</div>
+  }
 }
+
 export default App;
---
+++ src/components/welcome/welcome.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './welcome.scss';
+
+interface Welcome {
+  handleChange: any;
+  handleSubmit: any;
+}
+
+const Welcome: React.FC<Welcome> = ({ handleSubmit, handleChange }) => {
+  return (
+    <div className="welcome">
+      <h1>Welcome to Trivia!</h1>
+      <h4>Select the number of questions and hit go to begin</h4>
+      <form onSubmit={handleSubmit}>
+        <select name="numberOfQuestions" onChange={handleChange}>
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="15">15</option>
+        </select>
+        <input type="submit" value="Go" />
+      </form>
+    </div>
+  );
+};
+
+export default Welcome;
